1faf25ca20bc46becca55174e375d39cae060368
Vineet Agarwal <91052168+VineeTagarwaL-code@users.noreply.github.com>
2025-06-02 19:54:21 +0530

    feat: dist updated

--- examples/nextjs-example/pages/_app.tsx
+++ examples/nextjs-example/pages/_app.tsx
@@ -16,23 +16,15 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
+      {/*  */}
       <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function() {
-              var script = document.createElement('script');
-              script.src = "http://localhost:5173/dist/index.min.js";
-              script.onload = function() {
-                TranslationWidget("sk_ae757fe5d2d3f1faf73bb0139508fd45628c2218e06610095116ae01cacde5ac947edf3cd1bac990faa4dee5ee5e89747d2b12b3b38787716880bc3cab891b2c0244Zh7yv6Fmq7GUP1WSW", {
-                  pageLanguage: 'en',
-                  position: "top-right",
-                  autoDetectLanguage: false,
-                })
-              };
-              document.body.appendChild(script);
-            })();
-          `,
+        src="https://cdn.jsdelivr.net/gh/JigsawStack/translation-widget/dist/index.min.js"
+        onLoad={() => {
+          window.TranslationWidget("sk_ae757fe5d2d3f1faf73bb0139508fd45628c2218e06610095116ae01cacde5ac947edf3cd1bac990faa4dee5ee5e89747d2b12b3b38787716880bc3cab891b2c0244Zh7yv6Fmq7GUP1WSW", {
+            pageLanguage: 'en',
+            position: "top-right",
+            autoDetectLanguage: false,
+          });
         }}
       />
     </>
