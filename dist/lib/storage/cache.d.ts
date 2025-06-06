dfcabe3d76595ae2f0982d6cddc56bdd94e3c9a4
Vineet Agarwal <91052168+VineeTagarwaL-code@users.noreply.github.com>
2025-06-07 05:21:30 +0530

    feat: dist removed

--- dist/lib/storage/cache.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Cache {
-    cache: Map<any, any>;
-    maxSize: number;
-    constructor();
-    getKey(text: string, targetLang: string): string;
-    set(text: string, targetLang: string, translation: string): void;
-    get(text: string, targetLang: string): string | undefined;
-    clear(): void;
-}
