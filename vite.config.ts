4c35af50341bd43efcac32d9781a867ee4a86726
Vineet Agarwal <91052168+VineeTagarwaL-code@users.noreply.github.com>
2025-06-08 00:40:25 +0530

    feat: pkgroll cleared

--- vite.config.ts
+++ vite.config.ts
@@ -18,18 +18,11 @@ export default defineConfig({
         sourcemap: true,
         rollupOptions: {
             output: {
-                // Ensure the UMD build is minified
                 format: 'umd',
                 entryFileNames: '[name].min.js',
-                // Clean up the output directory on each build
-                chunkFileNames: '[name]-[hash].js',
-                // Ensure CSS is bundled with the JavaScript
-                assetFileNames: '[name][extname]',
             },
         },
-        // Ensure CSS is injected into the JavaScript bundle
         cssCodeSplit: false,
-        // Ensure CSS is inlined in the JavaScript bundle
         cssMinify: true,
     },
     plugins: [
