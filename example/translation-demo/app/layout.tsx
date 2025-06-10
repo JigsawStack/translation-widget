a191640e5eea8ec82bfa51cf8e2e1eb8279b99de
Vineet Agarwal <91052168+VineeTagarwaL-code@users.noreply.github.com>
2025-06-10 22:48:35 +0530

    feat: metadata updated

--- example/translation-demo/app/layout.tsx
+++ example/translation-demo/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import './globals.css'
 import TranslationWidget from "@/components/translationWidget";
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -14,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  // title: "Jigsaw Translation Widget Demo",
-  // description: "Experience seamless multilingual integration with Jigsaw's translation widget. Explore live demos for blogs, e-commerce, and more.",
+  title: "Jigsaw Translation Widget Demo",
+  description: "Experience seamless multilingual integration with Jigsaw's translation widget. Explore live demos for blogs, e-commerce, and more.",
 };
 
 export default function RootLayout({
