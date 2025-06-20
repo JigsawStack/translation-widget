fd47ed62920557bfc3b31621d5049d3f3735c59a
Vineet Agarwal <91052168+VineeTagarwaL-code@users.noreply.github.com>
2025-06-20 08:48:06 +0530

    feat: assertions updated

--- example/translation-demo-nextjs/components/translationWidget.tsx
+++ example/translation-demo-nextjs/components/translationWidget.tsx
@@ -4,6 +4,7 @@ import TranslationWidget from "translation-widget";
 
 export default function Translation() {
   useEffect(() => {
+    console.log("TranslationWidget", process.env.NEXT_PUBLIC_TRANSLATION_WIDGET_PUBLIC_KEY);
     TranslationWidget(process.env.NEXT_PUBLIC_TRANSLATION_WIDGET_PUBLIC_KEY!, {
       showUI: true,
       pageLanguage: 'en',
